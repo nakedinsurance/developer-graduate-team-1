5478853cdae586a6e58dd6e44c1516446624f406
J-Stanton <jameskstanton123@gmail.com>
2024-10-23 14:12:16 +0200

    feat(user-management): Removed code for private routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login.js';
 import Register from './components/Register.js';
-import PrivateRoute from './components/PrivateRoutes.js';
-
-const Dashboard = () => <h2>Dashboard: Protected Route</h2>;
 
 const App = () => {
   return (
@@ -13,14 +10,6 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route 
-          path="/dashboard" 
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          } 
-        />
       </Routes>
     </Router>
   );
