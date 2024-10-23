f4dc891552de01c7b7f492d18d157f8de5fe1056
J-Stanton <jameskstanton123@gmail.com>
2024-10-23 14:06:09 +0200

    feat(user-management): Added user login and registration using firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login.js';
+import Register from './components/Register.js';
+import PrivateRoute from './components/PrivateRoutes.js';
 
-function App() {
+const Dashboard = () => <h2>Dashboard: Protected Route</h2>;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } 
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
