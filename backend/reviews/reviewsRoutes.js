7844dc3b04100649f3b9dfd9af40dc22fd6ffab9
Karabo <karabo@Karabos-MacBook-Pro.local>
2024-10-23 15:07:35 +0200

    feat(reviews): post new reviews

--- backend/reviews/reviewsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getReviews } from 'reviewsController.js';
-
-const reviewRoutes = express.Router();
-
-// Get all reviews
-reviewRoutes.get('/all', getReviews);
-
-export default reviewRoutes;
